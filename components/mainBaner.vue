38ffb96b2166252b7ca0d6ada8eaeb29983b0750
Bruno Neves <dev@7br.uno>
2021-06-19 14:01:04 -0300

    first commit

---
+++ components/mainBaner.vue
@@ -0,0 +1,5 @@
+<template>
+    <v-container>
+        Teste
+    </v-container>  
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <main-baner />
+    </div>  
+</template>
+
+<script>
+import mainBaner from '~/components/mainBaner.vue'
+
+export default {
+  components: {
+    mainBaner
+  }
+}
+</script>
