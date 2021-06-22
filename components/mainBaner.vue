6b6704861592d727bb36655be49e1d9a708f6faf
Daniel Ribeiro <academicodan@gmail.com>
2021-06-21 22:59:00 -0300

    created components initial

--- components/mainBaner.vue
+++ components/mainBaner.vue
@@ -1,5 +1,37 @@
 <template>
-    <v-container>
-        Teste
-    </v-container>  
+  <div class="grey lighten-5">
+    <v-container py-16>
+      <v-row style="height: 100vh">
+        <v-col cols="12" sm="12" md="6"  lg="6" class="d-flex flex-column  align-center justify-center">
+          <div class="margen__decoration text-center">
+            <div class="indigo--text darken-4 body-1 mb-4">
+              Após 3 anos oferecendo conteúdo de MKT Digital para Corretores de
+              Seguros, entendemos que muitos precisam de um apoio maior.
+            </div>
+            <div class="mb-4">
+              As redes sociais e tudo que as envolvem, são uma bagagem extra para
+              o Corretor de seguros
+            </div>
+            <div class="mb-4">
+              Quando se propõe a subir o Everest, os alpinistas treinam, se
+              preparam, se equipam. Invariavelmente precisam de um Sherpa
+            </div>
+            <div>
+              Nós seremos o <b class="indigo--text darken-4">Sherpa</b> para este mercado
+            </div>
+          </div>
+        </v-col>
+         <v-col cols="12" sm="12" md="6"  lg="6" class="d-flex flex-column align-center justify-center">
+          <div>
+            IMG
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
+
+
+<style lang="scss" scoped>
+
+</style>
